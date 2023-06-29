67fd5dce141906158df449e8e8230a3dc7b112ad
Nicholas Sixbury <nico6bury@ksu.edu>
2023-06-29 16:30:18 -0500

    Added tabs to the main window. The flex containers are not behaving, as usual, but aside form some wasted space, everything is resizable and working.

--- src/gui.rs
+++ src/gui.rs
@@ -3,10 +3,13 @@ use fltk::app::App;
 use fltk::app::Receiver;
 use fltk::app::Sender;
 use fltk::button::Button;
+use fltk::enums::Align;
 use fltk::enums::FrameType;
 use fltk::enums::Shortcut;
 use fltk::group;
 use fltk::group::Flex;
+use fltk::group::Group;
+use fltk::group::Tabs;
 use fltk::menu;
 use fltk::menu::SysMenuBar;
 use fltk::prelude::FltkError;
@@ -31,13 +34,17 @@ pub struct GUI {
 	pub top_menu:SysMenuBar,
 	pub grid_buttons:Vec<Vec<Button>>,
 	pub grid_flex: FlexGrid,
+	pub tabs:Tabs,
+	pub settings_tab:Group,
+	pub districts_tab:Group,
 }//end struct gui
 
 fn get_default_win_width() -> i32 {900}
 fn get_default_win_height() -> i32 {480}
 fn get_default_menu_height() -> i32 {20}
+fn get_default_tab_padding() -> i32 {20}
 fn get_default_grid_width() -> i32 {get_default_win_width()}
-fn get_default_grid_height() -> i32 {get_default_win_height()-get_default_menu_height()}
+fn get_default_grid_height() -> i32 {get_default_win_height()-get_default_menu_height() - get_default_tab_padding()}
 
 impl GUI {
 	/// # default()
@@ -51,6 +58,9 @@ impl GUI {
 			top_menu:SysMenuBar::default(),
 			grid_buttons:Vec::new(),
 			grid_flex:FlexGrid::default(),
+			tabs:Tabs::default(),
+			settings_tab:Group::default(),
+			districts_tab:Group::default(),
 		};//end struct construction
 		gui.set_default_properties();
 		return gui;
@@ -69,6 +79,28 @@ impl GUI {
 		self.top_menu = self.top_menu.clone()
 			.with_size(get_default_win_width(), get_default_menu_height());
 		self.top_menu.set_frame(FrameType::FlatBox);
+		// actually make the menu show up
+		self.main_window.add(&self.top_menu);
+
+		// tabs settings
+		self.tabs = Tabs::new(0, get_default_menu_height(), get_default_win_width(), get_default_win_height(), None);
+		self.tabs.auto_layout();
+		self.tabs.end();
+		self.main_window.add(&self.tabs);
+
+		// settings tab
+		self.settings_tab = Group::new(0, self.tabs.y() + get_default_tab_padding(), self.tabs.width(), self.tabs.height(), "Settings");
+		self.settings_tab.end();
+		self.tabs.add(&self.settings_tab);
+
+		// district tab
+		self.districts_tab = Group::default()
+			.with_pos(0, self.tabs.y() + get_default_tab_padding())
+			.with_size(self.tabs.width(), self.tabs.height())
+			.with_label("Districts")
+			.with_type(group::FlexType::Row);
+		self.districts_tab.end();
+		self.tabs.add(&self.districts_tab);
 	}//end set_default_properties
 	/// # initialize_top_menu
 	/// 
@@ -96,8 +128,6 @@ impl GUI {
 			self.menu_send_receive.0.clone(),
 			MenuChoice::Resize,
 		);
-		// actually make the menu show up
-		self.main_window.add(&self.top_menu);
 	}//end initialize_top_menu
 	/// # initialize grid
 	/// 
@@ -105,12 +135,13 @@ impl GUI {
 	pub fn initialize_grid(&mut self, ext_grid:Grid<String>) {
 		// create a 2d array of buttons
 		let mut button_grid: Vec<Vec<Button>> = Vec::new();
+		// set size of buttons
+		let button_width = get_default_grid_width() / ext_grid.cols() as i32;
+		let button_height = get_default_grid_height() / ext_grid.rows() as i32;
 		for row_index in 0..ext_grid.rows() as i32 {
 			let mut temp_vec: Vec<Button> = Vec::new();
 			for col_index in 0..ext_grid.cols() as i32 {
 				// make the button, positioned correctly
-				let button_width = get_default_grid_width() / ext_grid.cols() as i32;
-				let button_height = get_default_grid_height() / ext_grid.rows() as i32;
 				let new_button = Button::default()
 					.with_size(button_width, button_height)
 					.with_label(ext_grid.get(row_index as usize, col_index as usize).unwrap());
@@ -125,9 +156,11 @@ impl GUI {
 		self.grid_flex.initialize_flex(ext_grid);
 		self.grid_flex.fill_flex(&self.grid_buttons);
 		// reposition flex grid because it likes to get lost
-		self.grid_flex.outer_flex.set_pos(0, self.top_menu.height());
+		// self.grid_flex.outer_flex.resize(0 - button_width / 2, self.districts_tab.y(), self.grid_flex.outer_flex.width() + button_width / 2, self.grid_flex.outer_flex.height());
 		// actually make the grid show up
-		self.main_window.add(&self.grid_flex.outer_flex);
+		self.districts_tab.add_resizable(&self.grid_flex.outer_flex);
+		// self.grid_flex.outer_flex.set_pos(0, self.districts_tab.y());
+		// self.grid_flex.outer_flex.set_pos(self.grid_flex.outer_flex.x() - button_width, self.grid_flex.outer_flex.y());
 	}//end initialize_grid
 	/// # show(self)
 	/// 
@@ -161,7 +194,7 @@ impl FlexGrid {
 	pub fn default() -> FlexGrid {
 		FlexGrid {
 			buttons:Vec::new(),
-			outer_flex:Flex::new(0, get_default_menu_height(), get_default_grid_width(), get_default_grid_height(), None),
+			outer_flex:Flex::new(0, get_default_menu_height() + get_default_tab_padding(), get_default_grid_width(), get_default_grid_height(), None),
 			inner_flexes:Vec::new(),
 		}//end struct construction
 	}//end new()
@@ -172,8 +205,9 @@ impl FlexGrid {
 	pub fn initialize_flex(&mut self, grid:Grid<String>) {
 		// set outer flex to be have rows of elements
 		self.outer_flex.set_type(group::FlexType::Row);
+		self.outer_flex.set_align(Align::LeftTop);
 		for _row_index in 0..grid.rows() {
-			let inner_flex_x = self.outer_flex.x();
+			let inner_flex_x = 0;//self.outer_flex.x();
 			let inner_flex_y = self.outer_flex.y() + (self.outer_flex.width() / grid.cols() as i32);
 			let inner_flex_w = get_default_grid_width() / grid.cols() as i32;
 			let inner_flex_h = get_default_grid_height() / grid.rows() as i32;
@@ -184,6 +218,9 @@ impl FlexGrid {
 			// save flex to struct
 			self.inner_flexes.push(inner_flex);
 		}//end adding inner flexes
+		println!("{} inner flexes", self.inner_flexes.len());
+		println!("inner flex x:{}", self.inner_flexes.first().unwrap().x());
+		println!("outer flex x:{}", self.outer_flex.x());
 	}//end initialize_flex(self, grid)
 
 	/// # fill_flex(self, buttons)
